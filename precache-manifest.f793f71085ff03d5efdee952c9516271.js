1610d0dca7998d1bacac8b0b1f60e3b44e41a25b
Omobolabo <107211043+Omobolabo@users.noreply.github.com>
2022-08-25 11:15:26 +0100

    Add files via upload

---
+++ precache-manifest.f793f71085ff03d5efdee952c9516271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b4dc56b8d1cc492aa19",
+    "url": "/static/css/main.e5929f56.chunk.css"
+  },
+  {
+    "revision": "6b4dc56b8d1cc492aa19",
+    "url": "/static/js/main.6b4dc56b.chunk.js"
+  },
+  {
+    "revision": "b03bffc27c43cb4a1669",
+    "url": "/static/js/1.b03bffc2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6271f40556b0578d1093678f8bcc29e8",
+    "url": "/index.html"
+  }
+];
